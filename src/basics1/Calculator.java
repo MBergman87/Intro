fad275199a9e3a389a133a318b216b5dcb84520a
javaclass <javaclass@15000500cpu.fec.org>
2017-09-26 15:19:11 -0500

    Addition

---
+++ src/basics1/Calculator.java
@@ -0,0 +1,12 @@
+package basics1;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+System.out.println(calculateSum(137,82));
+
+	}
+static int calculateSum(int a, int b) {
+	return a+b;
+}
+}
