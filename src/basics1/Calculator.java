f1110b4155bada615d9fff35d4054ac5860f6df2
javaclass <javaclass@15000500cpu.fec.org>
2017-09-27 13:53:55 -0500

    Updated Calculator

--- src/basics1/Calculator.java
+++ src/basics1/Calculator.java
@@ -9,4 +9,13 @@ System.out.println(calculateSum(137,82));
 static int calculateSum(int a, int b) {
 	return a+b;
 }
+static void divideSum (int a, int b) {
+	System.out.println(a/b);
+}
+static void subtractSum (int a, int b) {
+	System.out.println(a-b);
+}
+static void multiplySum (int a, int b) {
+	System.out.println(a*b);
+}
 }
